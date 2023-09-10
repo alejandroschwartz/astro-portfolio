3014d3f9ca3099ba13b1be7550f663a27f9c1404
Alejandro Schwartz <alejandro.schwartz@contaduria.marandu.com.ar>
2023-09-10 17:48:54 -0300

    Instalando, seteando el ServiceWorker y Manifest para crear la PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,38 +1,46 @@
-import { defineConfig } from "astro/config"
-
-// Astro integration imports
-import { VitePWA } from "vite-plugin-pwa"
-
-// Helper imports
-import { manifest, seoConfig } from "./seoConfig"
+import { defineConfig } from 'astro/config';
+import webmanifest from 'astro-webmanifest';
+import serviceWorker from "astrojs-service-worker";
 
 export default defineConfig({
-	site: seoConfig.baseURL,
-	// integrations: [
-	// 	tailwind({
-	// 		config: {
-	// 			applyBaseStyles: false,
-	// 			path: "./tailwind.config.js"
-	// 		}
-	// 	}),
-	// 	sitemap(),
-	// 	compress()
-	// ],
-	vite: {
-		plugins: [
-			VitePWA({
-				registerType: "autoUpdate",
-				manifest,
-				workbox: {
-				  globDirectory: 'dist',
-				  globPatterns: [
-				    '**/*.{js,css,svg,png,jpg,jpeg,gif,webp,woff,woff2,ttf,eot,ico}',
-				  ],
-				  // Don't fallback on document based (e.g. `/some-page`) requests
-				  // This removes an errant console.log message from showing up.
-				  navigateFallback: null,
-				},
-			})
-		]
-	}
-})
+  integrations: [
+	serviceWorker(),
+    webmanifest({
+      name: "Alejandro Schwartz Portfolio",
+      short_name: "Ale Schwartz",
+      description: "Portfolio de proyectos y Blog de Alejandro Schwartz",
+      lang: 'es-ES',
+      start_url: "/",
+      scope: "/",
+      theme_color: "#3367D6",
+      background_color: "#15b315",
+      display: "standalone",
+      prefer_related_applications: false,
+      related_applications: [],
+      icon: "public/favicon-32x32.png",
+      icons: [
+        {
+          "src": "public/maskable.png",
+          "sizes": "192x192",
+          "type": "image/png",
+          "purpose": "any maskable"
+        },
+        {
+          "src": "public/favicon.ico",
+          "sizes": "32x32 16x16",
+          "type": "image/x-icon"
+        },
+        {
+          "src": "public/android-chrome-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "public/android-chrome-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    }),
+  ],
+});
