364f35690974fc00c97da2ce9164f9b8d66b1bcf
Alejandro Schwartz <alejandro.schwartz@contaduria.marandu.com.ar>
2023-09-10 18:05:06 -0300

    Cambiando la configuracion del manifest, detalles

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,33 +12,32 @@ export default defineConfig({
       lang: 'es-ES',
       start_url: "/",
       scope: "/",
-      theme_color: "#3367D6",
-      background_color: "#15b315",
+      theme_color: "#ffffff",
+      background_color: "#ffffff",
       display: "standalone",
       prefer_related_applications: false,
       related_applications: [],
-      icon: "public/favicon-32x32.png",
       icons: [
         {
-          "src": "public/maskable.png",
-          "sizes": "192x192",
-          "type": "image/png",
-          "purpose": "any maskable"
+          src: "public/maskable.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "any maskable"
         },
         {
-          "src": "public/favicon.ico",
-          "sizes": "32x32 16x16",
-          "type": "image/x-icon"
+          src: "public/favicon.ico",
+          sizes: "32x32 16x16",
+          type: "image/x-icon"
         },
         {
-          "src": "public/android-chrome-192x192.png",
-          "sizes": "192x192",
-          "type": "image/png"
+          src: "public/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
         },
         {
-          "src": "public/android-chrome-512x512.png",
-          "sizes": "512x512",
-          "type": "image/png"
+          src: "public/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
         }
       ]
     }),
